337186b7bd770f5eae73bd02cd8ba0eebdd3a7fb
Gabriel Chiarel <gabrielchiarel@gmail.com>
2022-03-12 17:26:04 -0300

    Adicionar arquivos de projeto.

---
+++ MovieAPI/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> Movies = new();
+        private static int Id = 1;
+
+        [HttpPost]
+        public void AddMovie([FromBody]Movie movie)
+        {
+            movie.Id = Id++;
+            
+            Movies.Add(movie);
+
+            Console.WriteLine(movie.Title);
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return Movies;
+        }
+
+        [HttpGet]
+        public Movie GetMovieById(int id)
+        {
+            if (Movies.Any(x => x.Id == id))
+                return Movies.FirstOrDefault(x => x.Id == id);
+            else
+                return null;
+        }
+    }
+}
---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Director is required")]
+        public string Director { get; set; }
+
+        [Required(), StringLength(30, ErrorMessage = "MaxLength 30")]
+        public string Gender { get; set; }
+
+        [Range(1, 600, ErrorMessage = "Range 1 to 600")]
+        public int Duration { get; set; }
+    }
+}
